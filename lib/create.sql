5de211e141178af9081780d72c6b84a4919ebff9
Neematrasheed05 <nymatrasheed05@gmail.com>
2023-12-06 07:51:58 +0300

    Sqlite query

---
+++ lib/create.sql
@@ -0,0 +1,21 @@
+
+
+
+--id	integer
+-- name	text
+-- age	integer
+-- sex	text
+-- color	text
+-- temperament	text
+-- alive	boolean
+
+
+CREATE TABLE bears (
+  id INTEGER PRIMARY KEY,
+  name TEXT ,
+  age INTEGER,
+  sex TEXT,
+  color TEXT,
+  temperament TEXT,
+  alive BOOLEAN
+);
---
+++ lib/insert.sql
@@ -0,0 +1,8 @@
+INSERT INTO bears VALUES (1,"Mr. Chocolate", 20, "M", "dark brown", "clumsy", 0);
+INSERT INTO bears VALUES (2,"Rowdy", 4, "M", "white", "intense", 1);
+INSERT INTO bears VALUES (3,"Tabitha", 16, "F", "brown", "Nice", 0);
+INSERT INTO bears VALUES (4,"Sergeant Brown", 14, "M", "Green", "Slimy", 1);
+INSERT INTO bears VALUES (5,"Melissa", 31, "F", "dark brown", "goofy", 1);
+INSERT INTO bears VALUES (6,"Grinch", 2, "M", "Black", "Grinchy", 1);
+INSERT INTO bears VALUES (7,"Wendy", 12, "F", "Blue", "naive", 0);
+INSERT INTO bears VALUES (8,null, 20, "M", "black", "aggressive", 0);
